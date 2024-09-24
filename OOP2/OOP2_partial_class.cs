29e56eff73f544c3a71c0a35a4009e1a47385ae2
Roman <123767751+roma6kk@users.noreply.github.com>
2024-09-24 18:42:52 +0300

    Add files via upload

---
+++ OOP2/OOP2_partial_class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public partial class Vector
+{
+    public void Partial()
+    {
+        Console.WriteLine("Ключевое слово partial сработало");
+    }
+}
+namespace OOP2
+{
+    internal class OOP2_partial_class
+    {
+    }
+}
