473582930c804b08061dd26b5167f089e5b5986b
Roman <123767751+roma6kk@users.noreply.github.com>
2024-12-10 18:26:25 +0300

    Add files via upload

---
+++ OOP10/OOP10.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public partial class Vector { }
+
+namespace OOP10
+{
+    internal class OOP10
+    {
+        static void Main(string[] args)
+        {
+            string[] months =
+        {
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        };
+
+            int n = 4;
+            var monthsWithLengthN = months.Where(month => month.Length == n);
+            Console.WriteLine($"Месяцы состоящие из {n} смиволов:");
+            Console.WriteLine(string.Join(" ", monthsWithLengthN));
+
+
+            string[] summerMonths = { "June", "July", "August" };
+            string[] winterMonths = { "December", "January", "February" };
+
+            var summerAndWinterMonths = months.Where(month => summerMonths.Contains(month) || winterMonths.Contains(month));
+            Console.WriteLine("\nЛетние и зимние месяца:");
+            Console.WriteLine(string.Join(" ", summerAndWinterMonths));
+
+            var sortedMonths = months.OrderBy(month => month);
+            Console.WriteLine("\nМесяцы отсортированные в алфавитном порядке:");
+            Console.WriteLine(string.Join(" ", sortedMonths));
+
+            var monthsWithUAndLength = months.Where(month => month.Contains('u') && month.Length >= 4);
+            Console.WriteLine("\nМесяцы содержащие 'u' и длиной >= 4:");
+            Console.WriteLine(string.Join(" ", monthsWithUAndLength));
+
+            var joinLINQ = summerAndWinterMonths.Join(monthsWithUAndLength,monthsSAW => monthsSAW,uMonth => uMonth,(monthsSAW, uMonth) => monthsSAW);
+            Console.WriteLine("\njoinLINQ:");
+            Console.WriteLine(string.Join(" ", joinLINQ));
+
+            List<Vector> vectors = new List<Vector>();
+            vectors.Add(new Vector(new int[] { 1, 1 }));
+            vectors.Add(new Vector(new int[] { -1, -2, 3 }));
+            vectors.Add(new Vector(new int[] { 1, -2, 5, 5, 5 }));
+            vectors.Add(new Vector(new int[] { 99, 101, 7, 7, 7, 7, 7 }));
+            vectors.Add(new Vector(new int[] { 10, 0 }));
+            vectors.Add(new Vector(new int[] { 20, 0 }));
+            vectors.Add(new Vector(new int[] { 1, 1 }));
+            vectors.Add(new Vector(new int[] { 5, 5 }));
+            vectors.Add(new Vector(new int[] { 6, 6 }));
+            vectors.Add(new Vector(new int[] { 7, 7 }));
+            vectors.Add(new Vector(new int[] { 7, 8}));
+
+            int VectorsContainsZeroCounter = vectors.Count(v => v.Elements.Contains(0));
+            Console.WriteLine($"\nКоличество векторов, содержащих 0: {VectorsContainsZeroCounter}\n");
+            
+            double MinMagnitude = vectors.Min(v => v.GetMagnitude());
+            List<Vector> MinMagnitudeVectors = new List<Vector>();
+            MinMagnitudeVectors = vectors.Where(v => v.GetMagnitude() == MinMagnitude).ToList();
+            Console.Write("\nСписок векторов с наименьшим модулем: ");
+            Console.WriteLine(string.Join(" ", MinMagnitudeVectors) + '\n');
+
+            Vector[] VectorArray = vectors.Where(v => v.Length == 3 || v.Length == 5 || v.Length == 7).ToArray();
+            Console.Write("\nМассив векторов длины 3, 5, 7: ");
+            Console.WriteLine(string.Join(" ", VectorArray.ToList()));
+
+            double MaxMagnitude = vectors.Max(v => v.GetMagnitude());
+            Vector MaxMagnitudeVector = vectors.First(v => v.GetMagnitude() == MaxMagnitude);
+            Console.WriteLine($"\n\nМаксимальный вектор: {MaxMagnitudeVector}");
+
+            Vector FirstNegativeVector = vectors.First(v => v.Elements.Any(e => e < 0));
+            Console.WriteLine($"\nПервый вектор с отрицательным значением: {FirstNegativeVector}");
+
+            List<Vector> OrderedByLengthVectors = vectors.OrderBy(v => v.Length).ToList();
+            Console.Write("\nУпорядоченный список векторов по размеру: ");
+            Console.WriteLine(string.Join(" ", OrderedByLengthVectors));
+
+            var result = vectors
+           .Where(v => v.Elements.All(e => e >= 0)) 
+           .Select(v => new { Vector = v, Magnitude = v.GetMagnitude() }) 
+           .OrderByDescending(v => v.Magnitude) 
+           .GroupBy(v => v.Vector.Length) 
+           .Select(group => new 
+           {
+               Length = group.Key,
+               Count = group.Count(),
+               MaxMagnitude = group.Max(v => v.Magnitude),
+               Vectors = group.Select(g => g.Vector) 
+           });
+            foreach (var group in result)
+            {
+                Console.WriteLine("\nВекторы:");
+                foreach (var vector in group.Vectors)
+                {
+                    Console.WriteLine(vector);
+                }
+                Console.WriteLine($"Длина: {group.Length}, Количество: {group.Count}, Максимальный модуль: {group.MaxMagnitude}");
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ OOP10/OOP2.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+public partial class Vector
+{
+
+    public readonly int ID;
+    public const int const_field = 10;
+    private int Get_only { get; }
+    public static int InstanceCounter { get; set; }
+    private int Stat { get; set; }
+    public int[] Elements { get; set; }
+    public int Length { get; set; }
+
+public int this[int index]
+    {
+        get
+        {
+            if (index >= 0 && index < Length)
+            {
+                return Elements[index];
+            }
+            else
+            {
+                Stat = 2; 
+                return 0;
+            }
+        }
+        set
+        {
+            if (index >= 0 && index < Length)
+            {
+                Elements[index] = value;
+                Stat = 0;
+            }
+            else
+            {
+                Stat = 2; 
+            }
+        }
+    }
+
+    private Vector(bool isPrivate)
+    {
+        Elements = new int[] { 0 };
+        Length = 1;
+        Stat = 0;
+        Console.WriteLine("Был использован закрытый конструктор");
+    }
+    //static Vector()
+    //{
+    //    InstanceCounter = 0;
+    //    Console.WriteLine($"Статический конструктор инициализировал статическую переменную InstanceCounter = {InstanceCounter}");
+    //}
+    public Vector() 
+    {
+        Random rand = new Random();
+        Length = 5;
+        Elements = new int[Length];
+        for (int i = 0; i < Length; i++)
+        {
+            Elements[i] = rand.Next(1, 100); 
+        }
+        Stat = 0;
+        ID = GetHashCode();
+    }
+    public Vector(int[] elements)
+    {
+        if (elements != null && elements.Length > 0)
+        {
+            this.Elements = elements;
+            this.Length = elements.Length;
+            this.Stat = 0;
+            ID = GetHashCode();
+        }
+        else
+        {
+            this.Stat = 1;
+        }
+    }
+
+    public Vector(int size = 3, int status = 0, int[] elems = null)
+    {
+        if (elems == null || elems.Length != size)
+        {
+            Elements = new int[size];
+            Random rand = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                Elements[i] = rand.Next(1, 100);
+            }
+        }
+        else
+        {
+            Elements = elems;
+        }
+
+        Length = size;
+        ID = GetHashCode();
+        Stat = status;
+    }
+
+
+    public void Add(ref int value)
+    {
+        for (int i = 0; i < Length; i++)
+        {
+            Elements[i] += value;
+        }
+        value += 10;
+        Stat = 0;
+    }
+
+    public void Multiply(out int value)
+    {
+
+        value = 2;
+        for (int i = 0; i < Length; i++)
+        {
+            Elements[i] *= value;
+        }
+        Stat = 0; 
+    }
+
+    public void Print()
+    {
+        if (Stat == 0)
+        {
+            Console.WriteLine("Элементы вектора: ");
+            for (int i = 0; i < Length; i++)
+            {
+                Console.Write(Elements[i] + " ");
+            }
+            Console.WriteLine($"Количество экземпляров: {InstanceCounter}");
+        }
+        else
+        
+        {
+            Console.WriteLine($"Ошибка: код {Stat}");
+        }
+
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        Vector other = (Vector)obj;
+        if (Length != other.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < Length; i++)
+        {
+            if (Elements[i] != other.Elements[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public override int GetHashCode()
+    {
+        int hash = 17;
+        foreach (var element in Elements)
+        {
+            hash = hash * 31 + element;
+        }
+        InstanceCounter++;
+        return hash;
+    }
+
+    public override string ToString()
+    {
+        return $"Vector: [Length: {Length}, Elements: {string.Join(", ", Elements)}]";
+    }
+    //---------------- Задание 3
+    public bool ContainsZero()
+    {
+        return Elements.Contains(0);
+    }
+
+    public double GetMagnitude()
+    {
+        return Math.Sqrt(Elements.Sum(e => e * e));
+    }
+
+}
+
+namespace OOP2
+{
+    class OOP2
+    {
+        //    static void Main(string[] args)
+        //    {
+        //        //----------- Задание 1
+        //        int[] data = { 3, 4, 5, 6, 7 };
+        //        Console.WriteLine("Был использован конструктор с параметрами");
+        //        Vector vector = new Vector(data);
+        //        vector.Print();
+
+        //        Console.WriteLine("\nБыл использован конструктор без параметров");
+        //        Vector vectorB = new Vector();
+        //        Console.WriteLine($"ID: {vector.ID}");
+        //        vectorB.Print();
+        //        vectorB.Partial();
+
+        //        Console.WriteLine("\nБыл использован конструктор с параметрами по умолчанию");
+        //        Vector vectorC = new Vector(4);
+        //        vectorC.Print();
+        //        vectorC.Partial();
+
+        //        int value;
+        //        Console.WriteLine("\nВызван метод Multiply(), (выходной) параметр был передан по ссылке и инициализирован в методе");
+        //        vector.Multiply(out value);     
+        //        vector.Print();         
+
+        //        Console.WriteLine("\nВызван метод Add(), (входной) параметр передан по ссылке");
+        //        vector.Add(ref value);         
+        //        vector.Print();
+        //        //-------- Задание 2
+        //        Console.WriteLine($"\nvectorB равен vectorC? {vectorB.Equals(vectorC)}");
+        //        Console.WriteLine($"Тип vectorC: {vectorC.GetType()}");
+        //        //-------- Задание 3
+        //        Vector[] vectorArray = new Vector[]
+        //        {
+        //        new Vector(new int[] { 1, 2, 3 }),
+        //        new Vector(new int[] { 0, 4, 5 }),
+        //        new Vector(new int[] { 6, 7, 0 }),
+        //        new Vector(new int[] { 8, 9, 10 })
+        //        };
+
+        //        Console.WriteLine("\nВекторы, содержащие 0:");
+        //        foreach (var v in vectorArray.Where(v => v.ContainsZero()))
+        //        {
+        //            v.Print();
+        //        }
+
+        //        double minMagnitude = vectorArray.Min(v => v.GetMagnitude());
+        //        Console.WriteLine("\nВекторы с наименьшим модулем:");
+        //        foreach (var v in vectorArray.Where(v => v.GetMagnitude() == minMagnitude))
+        //        {
+        //            v.Print();
+        //        }
+        //        //-------- Задание 4
+        //        var vectorAnonymous = new
+        //        {
+        //            elements = new int[] { 1, 2, 3, 4, 5 },
+        //            length = 5,                  
+        //            stat = 0                     
+        //        };
+
+        //        Console.Write($"Значения анонимного типа: размер: {vectorAnonymous.length}," +
+        //            $" переменная состояния: {vectorAnonymous.stat}, элементы: ");
+        //        foreach (var element in vectorAnonymous.elements)
+        //        {
+        //            Console.Write(element + " ");
+        //        }
+        //        Console.WriteLine();
+        //        List<Vector> vectors = new List<Vector>();
+        //        vectors.Add(new Vector({ 0, 0 });
+        //        vectors.Add(new Vector({ data });
+        //        vectors.Add(new Vector(1, -2));
+        //        vectors.Add(new Vector(99, 101));
+        //        vectors.Add(new Vector(10, 0));
+        //        vectors.Add(new Vector(20, 0));
+        //        vectors.Add(new Vector(4, 4));
+        //        vectors.Add(new Vector(5, 5));
+        //        vectors.Add(new Vector(6, 6));
+        //        vectors.Add(new Vector(7, 7));
+        //    }
+        //}
+
+    }
+}
